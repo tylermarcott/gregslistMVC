894c1692b91b00938bccd3b0e641e56a1c38df12
Tyler Marcott <tylerjmarcott@gmail.com>
2023-08-23 14:57:54 -0600

    class instances created

--- app/models/House.js
+++ app/models/House.js
@@ -1,6 +1,17 @@
 
 
 
-class House {
-
+// NOTE remember to always export your class!
+export class House {
+  constructor(data) {
+    this.id = data.id || generateId()
+    this.year = data.year
+    this.name = data.name
+    this.bedrooms = data.bedrooms
+    this.bathrooms = data.bathrooms
+    this.sqft = data.sqft
+    this.price = data.price
+    this.description = data.description
+    this.imgUrl = data.imgUrl
+  }
 }
